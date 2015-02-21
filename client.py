0d7bd882f9dfa47c21eae6ae90fde8facd50662c
Carlos Bautista <cbautista@ise.com>
2015-02-21 19:40:10 +0000

    Add file write option

--- client.py
+++ client.py
@@ -1,8 +1,14 @@
+import sys
 from socket import socket
 import json
 
-j = json.dumps({'v':'pypy msg'})
+if sys.argv[1] == 'msg':
+    j = json.dumps({'v':'pypy msg'})
+    s = socket()
+    s.connect(('0.0.0.0', 18920))
+    s.send(j)
 
-s = socket()
-s.connect(('0.0.0.0', 18920))
-s.send(j)
+elif sys.argv[1] == 'stream':
+    f = open('py_output.log', 'a')
+    f.write(sys.argv[2]+'\n')
+    f.close()
